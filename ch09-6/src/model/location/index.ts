d576dab2267f581a0768d283b6ca138010e0e48d
illinia <67290425+illinia@users.noreply.github.com>
2021-06-16 20:16:53 +0300

    171~237

---
+++ ch09-6/src/model/location/index.ts
@@ -0,0 +1,5 @@
+import { ILocation } from "./ILocation";
+import { makeILocation } from "./makeILocation";
+import { makeRandomILocation } from "./makeRandomLocation";
+
+export { ILocation, makeILocation, makeRandomILocation };
