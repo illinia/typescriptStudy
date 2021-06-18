9cc6ac259ab65760af4a92281e713a466087fcb7
illinia <67290425+illinia@users.noreply.github.com>
2021-06-18 18:13:37 +0300

    294~339

---
+++ ch11-3/src/interfaces/index.ts
@@ -0,0 +1,9 @@
+import { IValuable } from "./IValuable";
+import { ISetoid } from "./ISetoid";
+import { IFunctor } from "./IFunctor";
+import { IApply } from "./IApply";
+import { IApplicative } from "./IApplicative";
+import { IChain } from "./IChain";
+import { IMonad } from "./IMonad";
+
+export { IValuable, ISetoid, IFunctor, IApply, IApplicative, IChain, IMonad };
---
+++ ch11-3/src/test/getRandomJoke-test.ts
@@ -0,0 +1,10 @@
+import * as R from "ramda";
+import { getRandomJoke, JokeType } from "../getRandomJoke";
+
+getRandomJoke()
+  .then((JokeItem: JokeType) => {
+    // @ts-ignore
+    const joke = R.view(R.lensProp("joke"))(JokeItem);
+    console.log(joke);
+  })
+  .catch((e: Error) => console.log(e.message));
